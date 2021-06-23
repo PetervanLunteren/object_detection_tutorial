40445f1ccc137a7a2cd5f9e59675c0166ed7794c
Peter van Lunteren <85185478+PetervanLunteren@users.noreply.github.com>
2021-06-23 22:20:18 +0200

    Add files via upload

--- change_folder_structure.py
+++ change_folder_structure.py
@@ -4,8 +4,11 @@ from pathlib import Path
 
 object_detection = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 shutil.rmtree(os.path.join(object_detection, "data"))
+print("Contents of folder 'data' removed :", os.path.join(object_detection, "data"))
 Path(os.path.join(object_detection, 'training')).mkdir(parents=True, exist_ok=True)
+print("Folder 'training' created :", os.path.join(object_detection, 'training'))
 Path(os.path.join(object_detection, 'images')).mkdir(parents=True, exist_ok=True)
+print("Folder 'images' created :", os.path.join(object_detection, 'images'))
 Path(os.path.join(object_detection, 'data')).mkdir(parents=True, exist_ok=True)
 
 current_dir = os.path.basename(os.path.normpath(os.path.dirname(os.path.abspath(__file__))))
